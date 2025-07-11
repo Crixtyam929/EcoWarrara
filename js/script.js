96fe8de0efa3bdf64f344ce3af03fecf6898510c
Darker <101016720+Crixtyam929@users.noreply.github.com>
2025-07-10 23:04:09 -0500

    Added JavaScript
    Added the basic JavaScript for animations

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let navbar = document.querySelector('.header .navbar');
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
+    btn.onclick = () =>{
+        let src = btn.getAttribute('data-src');
+        document.querySelector('.about .video-container .video').src = src;
+    }
+})
